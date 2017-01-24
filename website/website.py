29b4da6ae362f499e1eb6667f9be479e93cbad86
Aniruddha Achar B P <aniruddha.achar@gmail.com>
2017-01-24 15:01:18 +0530

    Added cache to the server.

--- website/website.py
+++ website/website.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from flask import Flask
@@ -5,15 +6,58 @@ from flask import flash
 from flask import render_template
 from flask import request
 from werkzeug.utils import secure_filename, redirect
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+from API.spotify_API import get_featured_songs
 from Machine_Learning.audio_feature_extraction import get_audio_features
+
 
 app = Flask(__name__)
 app.config.from_object('config')
 
+scheduler = BackgroundScheduler()
+
+global file_switch  # a variable used to switch between two different cache stored
+file_switch = 0
+
+
+def store_featured_playlist():
+    global file_switch
+    if 0 == file_switch:
+        fs = 1
+    else:
+        fs = 0
+    print(fs, file_switch)
+    featured = get_featured_songs()
+    js = json.dumps(featured)
+    if os.path.isfile(app.config['UPLOAD_FOLDER'] + 'featured_playlist' + str(file_switch) + '.json'):
+        with open(app.config['UPLOAD_FOLDER'] + 'featured_playlist{}.json'.format(fs), 'r') as fread:
+            if js == fread.read():
+                return
+    with open(app.config['UPLOAD_FOLDER'] + 'featured_playlist{}.json'.format(fs), 'w') as fpfile:
+        fpfile.write(js)
+        file_switch = fs
+
+scheduler.add_job(store_featured_playlist, 'interval', minutes=2, id='store_playlist')
+scheduler.start()
+
+atexit.register(lambda: scheduler.shutdown())
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    if not os.path.isfile(app.config['UPLOAD_FOLDER'] + 'featured_playlist' + str(file_switch) + '.json'):
+        print("File not exists")
+        store_featured_playlist()
+        with open(app.config['UPLOAD_FOLDER'] + 'featured_playlist' + str(file_switch) + '.json') as songs:
+            return songs.read()
+    if os.path.getsize(app.config['UPLOAD_FOLDER'] + 'featured_playlist' + str(file_switch) + '.json') <= 0:
+        print("File size zero")
+        store_featured_playlist()
+        with open(app.config['UPLOAD_FOLDER'] + 'featured_playlist' + str(file_switch) + '.json') as songs:
+            return songs.read()
+    with open(app.config['UPLOAD_FOLDER'] + 'featured_playlist' + str(file_switch) + '.json') as songs:
+        return songs.read()
 
 
 def allowed_file(filename):
@@ -31,7 +75,7 @@ def upload():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return "file saved successfully"+ get_audio_features(filename)
+            return "file saved successfully " + get_audio_features(filename)
     if request.method == 'GET':
         return render_template('upload.html')
 
