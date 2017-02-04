a69695624eb9b9f85142cb42fdf0dc4e9c94f899
Aniruddha Achar B P <aniruddha.achar@gmail.com>
2017-02-04 17:48:42 +0530

    NN ML testing

--- ML.py
+++ ML.py
@@ -16,6 +16,10 @@ from sklearn.multiclass import OneVsRestClassifier
 from sklearn import preprocessing
 from sklearn.externals import joblib
 
+from keras.models import Sequential
+from keras.layers import Dense
+
+
 key_activity_map = {1: "dinner", 2: "party", 3: "sleep", 4: "workout"}
 training_data_map = dict()
 testing_data_map = dict()
@@ -30,9 +34,8 @@ for key in training_data_map:
     X.extend(temp)
     for ele in temp:
         Y.append(key)
-
+'''
 min_max_scaler = preprocessing.MinMaxScaler()
-
 print(Y[-1])
 scaler = preprocessing.StandardScaler().fit(X)
 X = scaler.transform(X)
@@ -62,7 +65,7 @@ joblib.dump(Knnclf, 'kNeighbors_Classifier_track.pkl')
 joblib.dump(gnbclf, 'gaussian_NB_Classifier_track.pkl')
 joblib.dump(rfclf, 'random_forest_Classifier_track.pkl')
 joblib.dump(etclf, 'extra_tree_classifier_track.pkl')
-joblib.dump(scaler, 'scaler_track.pkl')
+joblib.dump(scaler, 'scaler_track.pkl')'''
 
 XTest = []
 YTrue = []
@@ -72,7 +75,7 @@ for key in testing_data_map:
     for ele in temp:
         YTrue.append(key)
 
-XTest = scaler.transform(XTest)
+'''XTest = scaler.transform(XTest)
 KnnYPred = Knnclf.predict(XTest)
 knnlist = KnnYPred.tolist()
 
@@ -110,4 +113,20 @@ print("GBaccuracy = {}% ".format(accuracy_score(YTrue, gbYpred) * 100))
 print("MCaccuracy = {}% ".format(accuracy_score(YTrue, mplclf) * 100))
 print("NNaccuracy = {}%".format(accuracy_score(YTrue,MLYpred)*100))
 print("NNaccuracy = {}%".format(accuracy_score(YTrue,MLYpred)*100))
-print("RF importance", rfclf.feature_importances_)
+'''
+# create model
+model = Sequential()
+model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
+model.add(Dense(8, init='uniform', activation='relu'))
+model.add(Dense(1, init='uniform', activation='sigmoid'))
+# Compile model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Fit the model
+model.fit(X, Y, nb_epoch=150, batch_size=10)
+# evaluate the model
+scores = model.evaluate(X, Y)
+#print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+classes = model.predict_classes(XTest, batch_size=32)
+loss_and_metrics = model.evaluate(XTest, YTrue, batch_size=32)
+print("Loss and metrics"+loss_and_metrics)
+print("Classes"+classes)
